ee03a069fbb6b7c38ffa7f06b4f7c22905ab54a7
Jacob Austin <jacobaustin@ymail.com>
2018-05-10 15:23:00 -0500

    First Commit

---
+++ ImageCheckr/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace ImageCheckr
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstFiles = new System.Windows.Forms.ListView();
+            this.filename = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.filelength = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.filesize = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Drag and drop the files to check here:";
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstFiles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.filename,
+            this.filelength,
+            this.filesize});
+            this.lstFiles.Location = new System.Drawing.Point(12, 25);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(590, 339);
+            this.lstFiles.TabIndex = 1;
+            this.lstFiles.UseCompatibleStateImageBehavior = false;
+            this.lstFiles.View = System.Windows.Forms.View.Details;
+            // 
+            // filename
+            // 
+            this.filename.Text = "Filename:";
+            this.filename.Width = 57;
+            // 
+            // filelength
+            // 
+            this.filelength.Text = "Filename length:";
+            this.filelength.Width = 89;
+            // 
+            // filesize
+            // 
+            this.filesize.Text = "File size:";
+            this.filesize.Width = 52;
+            // 
+            // frmMain
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 376);
+            this.Controls.Add(this.lstFiles);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.Text = "Image Checker";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.frmMain_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.frmMain_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView lstFiles;
+        private System.Windows.Forms.ColumnHeader filename;
+        private System.Windows.Forms.ColumnHeader filelength;
+        private System.Windows.Forms.ColumnHeader filesize;
+    }
+}
+
---
+++ ImageCheckr/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using iTextSharp.text.pdf;
+
+namespace ImageCheckr
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmMain_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) { e.Effect = DragDropEffects.Copy; }
+            
+        }
+
+        private void frmMain_DragDrop(object sender, DragEventArgs e)
+        {
+            bool longfound = false;
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+            {
+                string[] sizes = { "B", "KB", "MB", "GB", "TB" };
+                double len = new FileInfo(file).Length;
+                int order = 0;
+                while (len >= 1024 && order < sizes.Length - 1)
+                {
+                    order++;
+                    len = len / 1024;
+                }
+                if (Path.GetFileName(file).Length > Properties.Settings.Default.MaxFileLength)
+                {
+                    ListViewItem lsvitem = new ListViewItem(Path.GetFileName(file));
+                    lsvitem.SubItems.Add(Convert.ToString(Path.GetFileName(file).Length) + " characters");
+                    lsvitem.SubItems.Add(String.Format("{0:0.##} {1}", len, sizes[order]));
+                    lsvitem.ForeColor = Color.Red;
+                    lstFiles.Items.Insert(0, lsvitem);
+                    lstFiles.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+                    longfound = true;
+                }
+                else if ( len > Properties.Settings.Default.MaxFileSize && order == 2 || order >2)
+                {
+                    ListViewItem lsvitem = new ListViewItem(Path.GetFileName(file));
+                    lsvitem.SubItems.Add(Convert.ToString(Path.GetFileName(file).Length) + " characters");
+                    lsvitem.SubItems.Add(String.Format("{0:0.##} {1}", len, sizes[order]));
+                    lsvitem.ForeColor = Color.Red;
+                    lstFiles.Items.Insert(0, lsvitem);
+                    lstFiles.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+                    longfound = true;
+                }
+                else
+                {
+                    ListViewItem lsvitem = new ListViewItem(Path.GetFileName(file));
+                    lsvitem.SubItems.Add(Convert.ToString(Path.GetFileName(file).Length) + " characters");
+                    lsvitem.SubItems.Add(String.Format("{0:0.##} {1}", len, sizes[order]));
+                    lsvitem.ForeColor = Color.Green;
+                    lstFiles.Items.Add(lsvitem);
+                    lstFiles.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+                }
+
+
+            }
+            if (longfound == true) {
+                MessageBox.Show("Filename over 90 characters found, or size over 10mb!");
+            }
+        }
+    }
+}
